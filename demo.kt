b03e25a63569ff1759c221e9576b2f18fd6caff6
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-01 09:54:17 +0100

    source file

---
+++ demo.kt
@@ -0,0 +1,12 @@
+fun main(){
+    println("Hello World")
+    var language = "French"
+    val score = 95
+    println(score)
+    println(language)
+
+    val b = 2
+    val c = 4
+    val d = (b + c)
+    println(d)
+}
