a74d8db2f883478c481a00bcdc34639edb50dbd5
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-01 10:01:42 +0100

    source code

---
+++ maximum.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>){
+    val a = -9
+    val b = -11
+    val max = if (a>b){
+        println("$a is larger than $b")
+        println("max variable holds value of a")
+        a
+    }else{
+        println("$b is larger than $a")
+        println("max variable holds value of b")
+        b
+    }
+    println("max = $max")
+}
