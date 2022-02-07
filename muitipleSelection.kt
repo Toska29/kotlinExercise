48ed08b764d919faa88fafdbf18265395e159366
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-07 15:43:17 +0100

    multiple selection

---
+++ muitipleSelection.kt
@@ -0,0 +1,11 @@
+fun main(args:Array<String>){
+    var ch = 'A'
+    when(ch){
+        'A' -> println("A is a vowel")
+        'E' -> println("E is a vowel")
+        'I' -> println("I is a vowel")
+        'O' -> println("Ois a vowel")
+        'U' -> println("U is a vowel")
+        else -> println("$ch is not a vowel")
+    }
+}
