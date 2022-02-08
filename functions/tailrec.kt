ee2478c980b59f345c3fb47952b0bb36706169d8
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-08 23:58:52 +0100

    tail recursion

---
+++ functions/tailrec.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var number = 100000.toLong()
+    var result = recursiveSum(number)
+    println("sum of upto $number number = $result")
+}
+
+tailrec fun recursiveSum(n:Long, semiresult:Long = 0):Long{
+    return if(n <= 0){
+        semiresult
+    }else{
+        recursiveSum((n-1), n + semiresult)
+    }
+}
