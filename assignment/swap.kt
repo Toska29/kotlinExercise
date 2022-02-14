15d600273aa17e43583e3967c133d0bfc6f707c1
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-14 14:26:16 +0100

    swap algo

---
+++ assignment/swap.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var firstNumber:Int = 5
+    var secondNumber:Int = 6
+    var temp:Int
+
+    temp = firstNumber
+    firstNumber = secondNumber
+    secondNumber = temp
+
+    println("first number is $firstNumber")
+    println("second number is $secondNumber")
+
+}
