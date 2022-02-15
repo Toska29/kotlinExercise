2d4fdc02f86e8633e1e54e0486e93591891b90bc
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-15 10:06:33 +0100

    samples on array op

---
+++ arrays/arrayInOperator.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val cloudServices = arrayOf("Microsoft", "Amazon", "Google", "Heroku")
+    if("Amazon" in cloudServices){
+        println("Amazon exists in cloudservices")
+    }else{
+        println("Amazon does not exist in cloudservices")
+    }
+}
